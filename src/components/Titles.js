7e8ac5b2e8312dc3d37889f90ea2e226fe47364c
mrabenda92 <exo92mr@gmail.com>
2018-09-23 19:54:46 +0200

    beta

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Weather App</h1>
+                <p>Please select your country and city</p>
+            </div>
+        )
+    }
+};
+
+export default Titles;
